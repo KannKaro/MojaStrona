c0233dd68f4a25db63b48bb6a1c4164c96e3d7f6
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-11-27 11:15:29 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 let aboutMeBtn = document.querySelector('#aboutMeButton')
 let aboutMeContainer = document.querySelector('.container-about-me')
 let buttons = document.querySelector('.container-buttons')
+let backButton = document.querySelector('#backButton')
+let projectsButton = document.querySelector('#projectsButton')
 
 aboutMeBtn.addEventListener('click', () => {
     aboutMeContainer.style.display = 'flex'
     buttons.style.display = 'none'
+    backButton.style.display = 'inline'
+})
+
+backButton.addEventListener('click', () => {
+    aboutMeContainer.style.display = 'none'
+    buttons.style.display = 'flex'
+    backButton.style.display = 'none'
+})
+
+projectsButton.addEventListener('click', () => {
+    buttons.style.display = 'none'
+    backButton.style.display = 'inline'
 })
